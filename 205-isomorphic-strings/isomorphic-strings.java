bf2b023e2f959a2944bacc57a4fe7d97b66bbda8
dheeraj8780 <151668623+dheeraj8780@users.noreply.github.com>
2025-04-05 21:06:48 +0530

    Time: 8 ms (81.51%) | Memory: 42.8 MB (24.74%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int s1[]=new int[200];
+        int s2[]=new int[200];
+        for(int i=0;i<s.length();i++){
+            if(s1[s.charAt(i)] != s2[t.charAt(i)]){
+                return false;
+            }
+            s1[s.charAt(i)]=i+1;
+            s2[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
+}
